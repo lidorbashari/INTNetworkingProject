af1ae97870f1c4590c6fc9a0266619a89dc7f75c
Lidor Bashari <lidor1965@gmail.com>
2024-08-11 14:20:47 +0300

    TEST

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -3,6 +3,6 @@
 NEW_KEY=$(/home/ubuntu/.ssh/new_key)
 OLD_KEY=$(~/.ssh/old_key)
 ssh-keygen -t rsa -b 4096 -f ${OLD_KEY} -N ""
-scp ~/.ssh/authorized_keys ~/.ssh/id.rsa ubuntu@"${private_instance_ip}"
-ssh -i ~/.ssh/id.rsa ubuntu@"${private_instance_ip}" "cat ${new_key}.pub > ~/.ssh//autorized_keys && chmod 600 ~/.ssh//autorized_keys"
+scp "${NEW_KEY}.pub" ubuntu@"${private_instance_ip}:~/.ssh"
+ssh -i "${OLD_KEY}" ubuntu@"${private_instance_ip}" "cat ${new_key}.pub > ~/.ssh/autorized_keys && chmod 600 ~/.ssh/autorized_keys"
 cp ${NEW_KEY} ${OLD_KEY}
