1b2415a02c2fde8f4e703ac4f500f405f2f8f405
Lidor Bashari <lidor1965@gmail.com>
2024-08-12 12:59:25 +0300

    test

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-#Rotates the keys of the private instance
-CURRENT_KEY_PATH="/home/ubuntu/.ssh/barrotem-private-instance-key"
-ROLLED_KEY_PATH="/home/ubuntu/.ssh/barrotem-private-instance-key-rolled"
-private_server_ip=${1}
-
-if [[ -z private_server_ip ]]
-then
-  echo "Error : Private server ip unspecified."
+if [ -z "$1" ]; then
+  echo "Usage: $0 <private-instance-ip>"
   exit 1
-else
-  #Create new keyfile, overriding the previous one, redirecting output to /dev/null
-  ssh-keygen -q -f ${ROLLED_KEY_PATH} -N "" -t rsa <<< y > /dev/null
-  scp -q -i ${CURRENT_KEY_PATH} "${ROLLED_KEY_PATH}.pub" ubuntu@${private_server_ip}:"${ROLLED_KEY_PATH}.pub" #Copy new public key to private instance
-  ssh -i ${CURRENT_KEY_PATH} ubuntu@${private_server_ip} "cat ${ROLLED_KEY_PATH}.pub > ~/.ssh/authorized_keys" #Perform key rotation in public instance. ssh session will be disconnected.
-  cp ${ROLLED_KEY_PATH} ${CURRENT_KEY_PATH} #We're back to the public instance. Set current key to the rolled key.
 fi
+
+PRIVATE_IP=$1
+NEW_KEY_PATH="/home/ubuntu/.ssh/new_key"
+OLD_KEY_PATH="/home/ubuntu/.ssh/id_rsa"
+
+ssh-keygen -t rsa -b 4096 -f $NEW_KEY_PATH -N ""
+scp -i ${OLD_KEY_PATH} "${NEW_KEY_PATH}.pub" ubuntu@"${PRIVATE_INSTANCE_IP}":"~/.ssh/"
+
+ssh -i ${OLD_KEY_PATH} ubuntu@"${PRIVATE_INSTANCE_IP}" "cat ~/.ssh/new_key.pub > ~/.ssh/authorized_keys"
+
+ssh -i ${NEW_KEY_PATH} ubuntu@"${PRIVATE_INSTANCE_IP}" "echo 'New key works!'"
+
+ssh -i ${NEW_KEY_PATH} ubuntu@"${PRIVATE_INSTANCE_IP}" "rm ~/.ssh/new_key.pub"
